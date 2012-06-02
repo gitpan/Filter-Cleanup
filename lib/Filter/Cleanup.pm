23f71a034dfc58b0c51396a54558bd28936623b9
Jeff Ober <jeffober@gmail.com>
2012-06-02 02:00:39 +0000

    Import of JEFFOBER/Filter-Cleanup-0.02 from CPAN.
    gitpan-cpan-distribution: Filter-Cleanup
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         JEFFOBER/Filter-Cleanup-0.02.tar.gz
    gitpan-cpan-author:       JEFFOBER
    gitpan-cpan-maturity:     released

--- lib/Filter/Cleanup.pm
+++ lib/Filter/Cleanup.pm
@@ -1,6 +1,6 @@
 package Filter::Cleanup;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use Carp;
 use Filter::Util::Call;
@@ -8,7 +8,7 @@ use PPI;
 use PPI::Document;
 use PPI::Document::Fragment;
 
-use constant READ_BYTES => 999_999_999;
+use constant READ_BYTES => 999_999;
 
 sub import {
     my ($class, %args) = @_;
